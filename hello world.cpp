03de1d40ae8a5dfed5176e1a6a6fbbd856d7b0fe
Ashutosh Singh <ashutosh220kharayat@gmail.com>
2024-08-09 11:44:40 +0530

    Update hello world.cpp

--- hello world.cpp
+++ hello world.cpp
@@ -1,9 +1,14 @@
+// write a hello world program.
+// ASHUTOSH SINGH
+// 23070123033
+
 #include<iostream>
 using namespace std;
 
 int main(){
     cout<<"Hello world!";
 }
+
 // output
 /*Hello world!*/
 
